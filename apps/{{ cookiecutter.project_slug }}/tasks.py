ab1db39acba9e8f09dad6fc8093795a98ffc1c84
jifox <josef.fuchs@j-fuchs.at>
2023-03-05 06:55:32 +0100

    Initial commit for apps

--- apps/{{ cookiecutter.project_slug }}/tasks.py
+++ apps/{{ cookiecutter.project_slug }}/tasks.py
@@ -169,7 +169,8 @@ def docker_compose(context, command, **kwargs):
     compose_command = " ".join(compose_command_tokens)
     env = kwargs.pop("env", {})
     if "PYTHON_VER" not in env:
-        env["PYTHON_VER"] = context.{{cookiecutter.project_name}}.python_ver    if "NAUTOBOT_VER" not in env:
+        env["PYTHON_VER"] = context.{{cookiecutter.project_name}}.python_ver
+    if "NAUTOBOT_VER" not in env:
         env["NAUTOBOT_VER"] = context.{{cookiecutter.project_name}}.nautobot_ver
     if "hide" not in kwargs:
         print_command(compose_command, env=env)
@@ -330,7 +331,7 @@ def get_dependency_version(dependency_name):
     version_match = re.search(rf'^{dependency_name} = .*"[~^]?([0-9.]+)"', content, flags=re.MULTILINE)
     return version_match.group(1)
 
-{# 
+{#
 
 @task(
     help={
